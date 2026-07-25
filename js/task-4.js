688e8e4bb3fc9ccf82c93770167db3062f1f7362
Zelenskyi_Bohdan <zel.bohd@gmail.com>
2026-07-25 18:26:25 +0300

    added task 4 murkup

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,24 @@
+'use strict'
 
+const form = document.querySelector('.login-form');
+form.addEventListener('submit', validation);
+
+function validation(event) {
+    event.preventDefault();
+
+    const email = form.elements.email.value.trim();
+    const password = form.elements.password.value.trim();
+
+    if (email === '' && password === '') {
+        return alert('All form fields must be filled in');
+    } 
+
+    const values = {
+        email,
+        password,
+    };
+
+    console.log(values);
+
+    event.target.reset();
+}
